cfc77ef4faa4c6131bc9f38b2333caa405c87798
tic40 <ccpzjoh@gmail.com>
2022-06-09 09:50:02 +0900

    Format

--- src/mediator/chatroom.ts
+++ src/mediator/chatroom.ts
@@ -1,18 +1,18 @@
 export class ChatRoom {
-  logs: { date: Date, sender: User, message: string }[]
+  logs: { date: Date; sender: User; message: string }[]
 
   constructor() {
     this.logs = []
   }
 
   logMessage(user: User, message: string) {
-    const sender = user.getName();
+    const sender = user.getName()
     this.logs.push({ date: new Date(), sender: user, message })
   }
 
   displayLogs() {
-    for(const { date, sender, message } of this.logs) {
-      console.log(`${new Date().toLocaleString()} [${sender}]: ${message}`);
+    for (const { date, sender, message } of this.logs) {
+      console.log(`${new Date().toLocaleString()} [${sender}]: ${message}`)
     }
   }
 }
@@ -22,15 +22,15 @@ export class User {
   chatroom: ChatRoom
 
   constructor(name: string, chatroom: ChatRoom) {
-    this.name = name;
-    this.chatroom = chatroom;
+    this.name = name
+    this.chatroom = chatroom
   }
 
   getName() {
-    return this.name;
+    return this.name
   }
 
   send(message: string) {
-    this.chatroom.logMessage(this, message);
+    this.chatroom.logMessage(this, message)
   }
 }
--- tests/mediator.test.ts
+++ tests/mediator.test.ts
@@ -2,11 +2,11 @@ import { ChatRoom, User } from '../src/mediator/chatroom'
 
 test('mediator', () => {
   const logger = jest.fn()
-  const chatroom = new ChatRoom();
-  const user1 = new User("John Doe", chatroom);
-  const user2 = new User("Jane Doe", chatroom);
-  user1.send("Hi there!");
-  user2.send("Hey!");
+  const chatroom = new ChatRoom()
+  const user1 = new User('John Doe', chatroom)
+  const user2 = new User('Jane Doe', chatroom)
+  user1.send('Hi there!')
+  user2.send('Hey!')
   expect(chatroom.logs[0].sender).toBe(user1)
   expect(chatroom.logs[0].message).toBe('Hi there!')
   expect(chatroom.logs[1].sender).toBe(user2)
